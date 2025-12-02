d5abdf032ba005aa194115931f69121d150670e0
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-12-02 14:43:29 +0200

    Organize and document database operation code
    Added a `#region` directive to group code for adding and saving
    an employee to the database. Updated comments to provide clearer
    context for the commented-out code. No functional changes were made.

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -2,13 +2,15 @@
 
 var _dbContext = new AppDbContext();
 
+#region Add & Save To Database
 //var employee01 = new Employee
 //{
 //    Name = "Muhammad"
 //};
 
-//_dbContext.Employees.Add(employee01);
-//_dbContext.SaveChanges();
+//_dbContext.Employees.Add(employee01); -- Add Employee 
+//_dbContext.SaveChanges(); -- Save Employee to DataBase 
+#endregion
 
 
 
